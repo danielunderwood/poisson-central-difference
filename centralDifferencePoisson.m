4054fc8df22e432c004e9e8ca407b6cc3572ee57
Daniel Underwood <daniel.underwood13@gmail.com>
2015-04-27 01:28:38 -0400

    Fix domain expansion and not plotting out of domain points

--- centralDifferencePoisson.m
+++ centralDifferencePoisson.m
@@ -9,6 +9,18 @@ steps = round(1 / stepSize);
 % Expand domain using the kronecker tensor product
 expandedDomain = kron(domainMatrix, ones(steps));
 
+% Fix x axis missing expanded values and boundaries
+leftSide = kron(expandedDomain(:,1), ones(1, steps-1));
+leftBoundary = zeros(size(leftSide,1), 1);
+rightBoundary = leftBoundary;
+expandedDomain = [leftBoundary, leftSide, expandedDomain, rightBoundary];
+
+% Fix y axis missing expanded values and boundaries
+bottom = kron(expandedDomain(1,:), ones(steps-1, 1));
+topBoundary = zeros(1, size(bottom,2));
+bottomBoundary = topBoundary;
+expandedDomain = [bottomBoundary; bottom; expandedDomain; topBoundary];
+
 % Get x and y points by finding nonzero elements
 [yPoints, xPoints] = find(expandedDomain);
 
@@ -47,12 +59,19 @@ rhsVector = stepSize.^2 * rhs(xPoints, yPoints);
 solutionMatrix = operatorMatrix \ rhsVector;
 
 % Coordinates for Return
-[X, Y] = meshgrid(stepSize:stepSize:size(domainMatrix, 2), ...
-    stepSize:stepSize:size(domainMatrix, 1));
+[X, Y] = meshgrid(0:stepSize:size(domainMatrix, 2)+1, ...
+    0:stepSize:size(domainMatrix, 1)+1);
 
 Z = expandedDomain;
 for i=1:numel(solutionMatrix)
     Z(find(Z == 1, 1)) = solutionMatrix(i);
 end
+
+% Get rid of x and y coordinates not inside domain or boundary
+zeroIndices = find(~conv2(Z, [1 1 1; 1 0 1; 1 1 1], 'same'));
+X(zeroIndices) = NaN;
+Y(zeroIndices) = NaN;
+    
+
 end
 
